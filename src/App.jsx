b3e6603147bd7aa6ec3f4446bdb9c0d6e830e824
shodeq <alfiananwarshodiqi@gmail.com>
2024-09-19 19:52:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="bg-blue-500 text-white p-4 w-full h-screen flex justify-center items-center flex-col gap-5">
+            <h1>ini brach main</h1>
+        </div>
+    );
+}
